d57bbb9c7f9ba7c7c74d13c7df28fc9fc38f87e0
cammarcode <nzcamm@gmail.com>
2023-07-30 10:14:57 +1200

    added matterdims.js

---
+++ matterdims.js
@@ -0,0 +1,12 @@
+
+class MatterDim{
+    constructor(basemult, basecost, basescaling) 
+    { 
+        this.mult = basemult;
+        this.cost = basecost;
+        this.scaling = basescaling;
+        this.count = 0
+     }
+     
+
+}
